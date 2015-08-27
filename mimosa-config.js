c027ee206b8f3f00d939b53e24f2b5316df36d75
Adam Krawiec <adamkrawiec@gmail.com>
2015-08-26 19:52:52 -0500

    remove dupes in config, reformat code

--- mimosa-config.js
+++ mimosa-config.js
@@ -1,64 +1,49 @@
 
 exports.config = {
-    "modules": [
-        "copy",
-        "server",
-        "coffeescript",
-        // "coffeelint",
-        // "csslint",
-        "lint",
-        "require",
-        "minify-js",
-        "minify-css",
-        'minify-img',
-        "live-reload",
-        "bower",
-        "jade",
-        "server-template-compile",
-        "less",
-        "dependency-graph"
-    ],
-    "iced": {
-        extensions: ["coffee", "iced"],
-        options: {
-            sourceMap: true,
-            bare: true,
-            runtime: "inline"
-        }
+  'modules': [
+    'copy',
+    'server',
+    'coffeescript',
+    // 'coffeelint',
+    // 'csslint',
+    'lint',
+    'require',
+    'minify-js',
+    'minify-css',
+    'minify-img',
+    'live-reload',
+    'bower',
+    'jade',
+    'server-template-compile',
+    'less',
+    'dependency-graph'
+  ],
+  serverTemplate: {
+    locals: {
+      optimize: false,
+      build: true
+    }
+  },
+  require: {
+    exclude:[],
+    commonConfig: 'common',
+    tracking: {
+      enabled: true,
+      path: '.mimosa/require/tracking.json'
     },
-    "serverTemplate": {
-        'locals': {
-            "title": "Mimosa",
-            "reload": false,
-            "optimize": true,
-            "build": true
-        }
-    },
-    serverTemplate: {
-      locals: {
-        optimze: false
+    verify: {
+      enabled: true,
+      plugins: {
+        css: 'css',
+        hbs: 'hbs'
       }
     },
-    require: {
-      exclude:[],
-      commonConfig:"common",
-      tracking: {
-        enabled: true,
-        path: ".mimosa/require/tracking.json"
-      },
-      verify: {
-        enabled: true,
-        plugins: {
-          css:"css",
-          hbs:"hbs"
-        }
-      },
-      optimize : {
-        modules: null,
-        moduleCachingPath: ".mimosa/require/moduleCaching",
-        inferConfig:true,
-        removeCombined: true,
-        overrides:{}
-      }
+    optimize : {
+      modules: null,
+      moduleCachingPath: '.mimosa/require/moduleCaching',
+      inferConfig: true,
+      removeCombined: true,
+      overrides: {}
     }
   }
+}
